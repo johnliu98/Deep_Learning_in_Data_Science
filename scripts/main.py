594ebe329f5477578eea769b7a28065aef1ce4e0
johnliu98 <68383339+johnliu98@users.noreply.github.com>
2021-05-19 13:37:37 +0200

    Added backward and accuracy method to model

---
+++ scripts/main.py
@@ -0,0 +1,64 @@
+
+import torch
+import torchvision
+from torchvision import transforms, datasets
+from torch.utils.data import Subset, DataLoader
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from model import ShuffleNet
+
+# Set random seed
+np.random.seed(0)
+torch.manual_seed(0)
+
+# Run on GPU if possible, otherwise run on CPU
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on GPU...")
+else:
+    device = torch.device("cpu")
+    print("Running on CPU...")
+
+CLASSES = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+EPOCHS = 1
+BATCH_SIZE = 100
+GROUPS = 1
+
+def load_cifar10_data():
+    # Upload data
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    dataset = datasets.CIFAR10(root='./data', train=True,
+                                download=True, transform=transform)
+
+    inds = torch.randperm(len(dataset))
+    train_inds = inds[:45000]
+    val_inds = inds[45000:]
+
+    trainset = Subset(dataset, train_inds)
+    valset = Subset(dataset, val_inds)
+    testset = datasets.CIFAR10(root='./data', train=False,
+                               download=True, transform=transform)
+
+    trainloader = DataLoader(trainset, batch_size=BATCH_SIZE,
+                             shuffle=True, num_workers=2)
+    valloader = DataLoader(valset, batch_size=BATCH_SIZE,
+                           shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=BATCH_SIZE,
+                            shuffle=False, num_workers=2)
+
+    return trainloader, valloader, testloader
+
+if __name__ == "__main__":
+    trainloader, valloader, testloader = load_cifar10_data()
+
+    net = ShuffleNet(groups=GROUPS, num_classes=len(CLASSES))
+    net.backward(trainloader, valloader, epochs=EPOCHS)
+    net.accuracy(testloader)
+
--- scripts/model.py
+++ scripts/model.py
@@ -1,44 +1,17 @@
 import os
 import numpy as np
-from tqdm import tqdm
-import pandas as pd
-import matplotlib.pyplot as plt
+from time import time
 from collections import OrderedDict
 
 import torch
+import torchvision
+import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-import torchvision
-from torchvision import transforms, datasets
-
-# Set random seed
-np.random.seed(0)
-torch.manual_seed(0)
-
-# Run on GPU if possible, otherwise run on CPU
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    print("Running on GPU...")
-else:
-    device = torch.device("cpu")
-    print("Running on CPU...")
-
-# Get MNIST dataset
-train = datasets.MNIST('', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
-
-test = datasets.MNIST('', train=False, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ]))
-
-trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
-testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=False)
-
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def channel_shuffle(x, groups):
     batchsize, channels, height, width = x.data.size()
@@ -96,29 +69,21 @@ class ShuffleUnit(nn.Module):
         self.first_1x1_groups = self.groups
 
         self.g_conv_1x1_compress = self._make_grouped_conv1x1(
-            self.in_channels,
-            self.bottleneck_channels,
-            groups=self.first_1x1_groups,
-            batch_norm=True,
-            relu=True)
+            self.in_channels, self.bottleneck_channels,
+            groups=self.first_1x1_groups, batch_norm=True, relu=True
+        )
 
         # 3x3 depthwise convolution followed by batch normalization
         self.depthwise_conv3x3 = self._make_grouped_conv3x3(
-            self.bottleneck_channels,
-            self.bottleneck_channels,
-            groups=self.bottleneck_channels,
-            padding=1,
-            batch_norm=True
+            self.bottleneck_channels, self.bottleneck_channels,
+            groups=self.bottleneck_channels, padding=1, batch_norm=True
         )
 
         # Use 1x1 grouped convolution to expand from
         # bottleneck_channels to out_channels
         self.g_conv_1x1_expand = self._make_grouped_conv1x1(
-            self.bottleneck_channels,
-            self.out_channels,
-            self.groups,
-            batch_norm=True,
-            relu=False
+            self.bottleneck_channels, self.out_channels,
+            groups=self.groups, batch_norm=True, relu=False
         )
 
     @staticmethod
@@ -157,12 +122,13 @@ class ShuffleUnit(nn.Module):
 
         conv = nn.Conv2d(in_channels, out_channels,
                          kernel_size=3,
+                         stride=self.depthwise_stride,
                          groups=groups,
                          padding=padding)
         modules['conv'] = conv
 
         if batch_norm:
-            modules['batch_norm'] = batch_norm
+            modules['batch_norm'] = nn.BatchNorm2d(out_channels)
         if len(modules) > 1:
             return nn.Sequential(modules)
         else:
@@ -182,34 +148,202 @@ class ShuffleUnit(nn.Module):
         out = self._combine_func(residual, out)
         return F.relu(out)
 
-class ShuffleNet(nn.Module):
 
-    def __init__(self):
+class ShuffleNet(nn.Module):
+    """ShuffleNet implementation.
+    """
+
+    def __init__(self, groups=3, in_channels=3, num_classes=10):
+        """ShuffleNet constructor.
+
+        Arguments:
+            groups (int, optional): number of groups to be used in grouped
+                1x1 convolutions in each ShuffleUnit. Default is 3 for best
+                performance according to original paper.
+            in_channels (int, optional): number of channels in the input tensor.
+                Default is 3 for RGB image inputs.
+            num_classes (int, optional): number of classes to predict. Default
+                is 1000 for ImageNet.
+
+        """
         super(ShuffleNet, self).__init__()
 
-        # Define network layers here:
-        # ...
+        self.groups = groups
+        self.stage_repeats = [3, 7, 3]
+        self.in_channels = in_channels
+        self.num_classes = num_classes
 
+        # index 0 is invalid and should never be called.
+        # only used for indexing convenience.
+        if groups == 1:
+            self.stage_out_channels = [-1, 24, 144, 288, 567]
+        elif groups == 2:
+            self.stage_out_channels = [-1, 24, 200, 400, 800]
+        elif groups == 3:
+            self.stage_out_channels = [-1, 24, 240, 480, 960]
+        elif groups == 4:
+            self.stage_out_channels = [-1, 24, 272, 544, 1088]
+        elif groups == 8:
+            self.stage_out_channels = [-1, 24, 384, 768, 1536]
+        else:
+            raise ValueError(
+                """{} groups is not supported for
+                   1x1 Grouped Convolutions""".format(groups))
 
-    def make_data(self):
-        # Extract training data
-        data = None
-        return data
+        # Stage 1 always has 24 output channels
+        self.conv1 = nn.Conv2d(
+            self.in_channels, self.stage_out_channels[1],
+            kernel_size=3, stride=3, padding=1
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-    def convs(self, x):
-        # Define the feed forward convolutional part, aka
-        # the network architecture
+        # Stage 2
+        self.stage2 = self._make_stage(2)
+        # Stage 3
+        self.stage3 = self._make_stage(3)
+        # Stage 4
+        self.stage4 = self._make_stage(4)
 
-        return x
+        # Global pooling:
+        # Undefined as PyTorch's functional API can be used for on-the-fly
+        # shape inference if input size is not ImageNet's 224x224
+
+        # Fully-connected classification layer
+        num_inputs = self.stage_out_channels[-1]
+        self.fc = nn.Linear(num_inputs, self.num_classes)
+        self.init_params()
+
+        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+        self.loss_function = nn.NLLLoss()
+
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def _make_stage(self, stage):
+        modules = OrderedDict()
+        stage_name = "ShuffleUnit_Stage{}".format(stage)
+
+        # First ShuffleUnit in the stage
+        # 1. non-grouped 1x1 convolution (i.e. pointwise convolution)
+        #   is used in Stage 2. Group convolutions used everywhere else.
+        grouped_conv = stage > 2
+
+        # 2. concatenation unit is always used.
+        first_module = ShuffleUnit(
+            self.stage_out_channels[stage - 1],
+            self.stage_out_channels[stage],
+            groups=self.groups,
+            grouped_conv=grouped_conv,
+            combine='concat'
+        )
+        modules[stage_name + "_0"] = first_module
+
+        # add more ShuffleUnits depending on pre-defined number of repeats
+        for i in range(self.stage_repeats[stage - 2]):
+            name = stage_name + "_{}".format(i + 1)
+            module = ShuffleUnit(
+                self.stage_out_channels[stage],
+                self.stage_out_channels[stage],
+                groups=self.groups,
+                grouped_conv=True,
+                combine='add'
+            )
+            modules[name] = module
+
+        return nn.Sequential(modules)
 
     def forward(self, x):
-        x = self.convs(x)
+        x = self.conv1(x)
+        x = self.maxpool(x)
 
-        # Define method for the fully-connected layer
-        return x
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
 
-    def test(self):
-        # Compute accuracy
-        accuracy = 0
-        return accuracy
+        # global average pooling layer
+        x = F.avg_pool2d(x, x.data.size()[-2:])
 
+        # flatten for input to fully-connected layer
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return F.log_softmax(x, dim=1)
+
+    def backward(self, trainloader, valloader, epochs=1):
+
+        train_losses = []
+        val_losses = []
+        for epoch in range(epochs):
+
+            train_loss = 0.0
+            for i, data in enumerate(tqdm(trainloader)):
+                X_batch, y_batch = data
+
+                self.zero_grad()
+
+                y_pred = self(X_batch)
+                loss = self.loss_function(y_pred, y_batch)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                train_loss += loss.item()
+                if i % 10 == 9:  # print every 100 mini-batches
+                    train_losses.append(train_loss / 10)
+
+                    with torch.no_grad():
+                        tot_val_loss = 0.0
+                        for val_data in valloader:
+                            X_val, y_val = val_data
+                            N_val = len(valloader)
+
+                            y_val_pred = self(X_val)
+                            val_loss = self.loss_function(y_val_pred, y_val)
+                            tot_val_loss += val_loss.item()
+                        val_losses.append(tot_val_loss / N_val)
+
+                    print('[%d, %5d] train_loss: %.3f, val_loss: %.3f' %
+                          (epoch + 1, i + 1, train_loss / 10, tot_val_loss / N_val))
+                    train_loss = 0.0
+
+        plt.plot(train_losses)
+        plt.plot(val_losses)
+        plt.show()
+
+        print('Finished Training')
+
+    def accuracy(self, testloader):
+        correct = 0
+        total = 0
+        # since we're not training, we don't need to calculate the gradients for our outputs
+        with torch.no_grad():
+            for data in tqdm(testloader):
+                X_test, y_test = data
+                # calculate outputs by running images through the network
+                y_pred = self(X_test)
+                # the class with the highest energy is what we choose as prediction
+                _, predicted = torch.max(y_pred.data, 1)
+                total += y_test.size(0)
+                correct += (predicted == y_test).sum().item()
+
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+                100 * correct / total))
+
+    def save_network(self):
+        path = './networks/cifar_shufflenet.pth'
+        path = path[:-4] + str(int(time())) + path[-4:]
+        torch.save(self.state_dict(), path)
+
+    def load_network(self, path, device):
+        self.load_state_dict(torch.load(path, device))
